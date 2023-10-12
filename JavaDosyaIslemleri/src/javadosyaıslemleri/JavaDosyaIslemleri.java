5313a610be7fb7c9d430c7a062d98aa300293baf
bilalYalcindag <121752531+bilalYalcindag@users.noreply.github.com>
2023-10-12 19:05:04 +0300

    read and write
    documentation read and write process is completed.

--- JavaDosyaIslemleri/src/javadosyaıslemleri/JavaDosyaIslemleri.java
+++ JavaDosyaIslemleri/src/javadosyaıslemleri/JavaDosyaIslemleri.java
@@ -4,6 +4,13 @@
  */
 package javadosyaıslemleri;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author acer
@@ -13,8 +20,30 @@ public class JavaDosyaIslemleri {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException{
+        File file = new File("kod.txt");
+        
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        
+        String word = "Dosya yazma denemesi" ; 
+        
+        FileWriter fileWriter = new FileWriter(file, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(word);
+        bufferedWriter.close();
+        
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        
+        String line;
+        
+            while((line = bufferedReader.readLine())!=null){
+                System.out.println(line); 
+            }
+        bufferedReader.close();
+        
     }
     
 }
